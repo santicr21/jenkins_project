4fd329a07751b71ed38823029891b38b5ad48c82
Santiago Caicedo Rojas <santiago.caicedo@globant.com>
2023-08-30 14:04:06 -0500

    Added: Reverse the name

--- HelloEnv.java
+++ HelloEnv.java
@@ -2,5 +2,18 @@ public class HelloEnv {
     public static void main (String[] args) {
         String systemVariable = System.getenv("MY_NAME");
         System.out.println("My name is " + systemVariable);
+
+        if (systemVariable != null) {
+            String reversed = "";
+            int n = systemVariable.length();
+
+            for(int i = 0; i < n; i++){
+                reversed = systemVariable.charAt(i) + reversed;
+            }
+
+            System.out.println("My name have been reversed: " + reversed);
+        }
+
+
     }
 }
